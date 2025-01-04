04fffc4b111bc8d9c2ff16d5d224c3354ffb571d
rupinderjdn <82199133+rupinderjdn@users.noreply.github.com>
2025-01-04 18:32:44 +0530

    programatic navigation

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,9 +1,21 @@
+'use client'
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const newUserPage = () => {
+  const router = useRouter();
+
+
   return (
     <div>
+
       <h1>New User</h1>
+      <button className='btn btn-primary' onClick={() => {
+        console.log("clicked");
+        router.push("/users");
+      }}> 
+           Users
+         </button>
     </div>
   )
 }
